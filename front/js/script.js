84534327e453f0076200903f7f4919b0ade09f0e
Chris <nico.bar2012@gmail.com>
2021-01-25 22:15:52 +0100

    feat : front-end add html code add css add js add materialize

---
+++ front/js/script.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(".dropdown-trigger").dropdown();
+
+$(document).ready(function(){
+  $('.tabs').tabs()
+});
