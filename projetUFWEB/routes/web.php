e2493e22b228ad7575f8bb6c7085af39c9e73064
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-01-25 22:37:31 +0100

    suppression des routes, nouveau déepart plus propre

--- projetUFWEB/routes/web.php
+++ projetUFWEB/routes/web.php
@@ -18,25 +18,3 @@ use App\Http\Controllers\Article;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/users', [UserController::class, 'index']);
-
-Route::get('article', function () {
-    return view('article.index');
-});
-
-//création d'url pour les articles suivant l'identifiant donné
-//il reste à faire l'ajout d'id par l'extérieur (avec la base de donné et le header clickable)
-//et la page html (front) à finir
-Route::get('projet/{article}', function ($articleId) {
-    return view('projet');
-    //'Article : ' . $articleId;
-})->where('article', '[0-9]+');
-
-
-//création d'url pour les utilisateurs suivant l'identifiant donné
-//il reste à faire l'ajout d'id par l'extérieur (avec la base de donné et le header clickable)
-//et la page html (front) à finir
-Route::get('user/{user}', function ($UserId) {
-    return 'Page utilisateur : ' . $UserId;
-})->where('article', '[0-9]+');
