2cee3db9b38d67a272a8515a6d86aa0b18855cfa
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-01-26 17:15:54 +0100

    ajout des pages signin et login, vide

---
+++ projetUFWEB/resources/views/signin.blade.php
@@ -0,0 +1,14 @@
+<!--utiliser la vue parent-->
+@extends('layouts.app')
+
+<!-- Choix du titre -->
+@section('title', 'S'enregistrer')
+
+@section('top')
+@endsection
+
+@section('center')
+@endsection
+
+@section('foot')
+@endsection
