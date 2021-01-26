37fa0b079c09d9397bdf0bda3514da8e5c5ca9bc
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-01-26 23:26:24 +0100

    test CSS, echec

--- projetUFWEB/resources/views/layouts/app.blade.php
+++ projetUFWEB/resources/views/layouts/app.blade.php
@@ -2,19 +2,10 @@
 <html lang="fr">
 <head>
     <title>@yield('title')</title> 
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="../css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="">
-    <title>Projet UF</title>
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="app.css"/>
 </head>
 <body>
     @component('layouts.components.navbar')
