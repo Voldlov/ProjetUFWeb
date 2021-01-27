0b69b4450a885d29e57923d104d23ad3f5a5c990
Chris <nico.bar2012@gmail.com>
2021-01-27 15:39:40 +0100

    update : listing add listing.html update css update js update profil.html

--- front/js/script.js
+++ front/js/script.js
@@ -10,6 +10,26 @@
 
 $(".dropdown-trigger").dropdown();
 
+//tab function Materialize
 $(document).ready(function(){
   $('.tabs').tabs()
+});
+
+//select fonction Materialize
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+//autocomplete function Materialize
+$(document).ready(function(){
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Marne-la-Vallée": null,
+      "Osny": null,
+      "Cergy Préfecture": null,
+      "Nanterre": null,
+      "Monaco" : null,
+      "Minas Tirith" : null,
+    },
+  });
 });
