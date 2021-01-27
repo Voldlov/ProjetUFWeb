8b3f1968209a05188f357a9b0ad8d80f02d9501d
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-01-27 07:45:14 +0100

    modification permetant au CSS et au JS de fonctionner. Pour cela, fichier mit dans public.

--- projetUFWEB/resources/views/layouts/app.blade.php
+++ projetUFWEB/resources/views/layouts/app.blade.php
@@ -5,7 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="">
-    <link rel="stylesheet" href="app.css"/>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
     @component('layouts.components.navbar')
@@ -36,6 +45,6 @@
 <!--JavaScript at end of body for optimized loading-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-    <script type="text/javascript" src="../js/materialize.min.js"></script>
-    <script src="../js/script.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script src="js/script.js"></script>
 </html>
